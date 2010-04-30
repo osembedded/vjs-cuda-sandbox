5436a63c4b2410082fc8c7a2af1a07519e37adbf
vijay.jayaraman <vijay.jayaraman@082e103b-4302-d31d-8b44-42596f57890b>
2010-04-30 23:08:55 +0000

    More cleanup and modularization...

--- cuda/img_transform/blacknwhite/blacknwhite.cu
+++ cuda/img_transform/blacknwhite/blacknwhite.cu
@@ -17,13 +17,36 @@ UINT8 *B;
 UINT8 *convR;
 UINT8 *convG;
 UINT8 *convB;
+UINT8 *infile;
 UINT8 *outfile;
+
+#define PARAMS_GOOD                             \
+   (NULL != header &&                           \
+    NULL != R &&                                \
+    NULL != G &&                                \
+    NULL != B &&                                \
+    NULL != convR &&                            \
+    NULL != convB &&                            \
+    NULL != convG &&                            \
+    NULL != infile &&                           \
+    NULL != outfile)
+
+#define FREE_MEM                                \
+   free(header);                                \
+   free(R);                                     \
+   free(G);                                     \
+   free(B);                                     \
+   free(convR);                                 \
+   free(convG);                                 \
+   free(convB);                                 \
+   free(infile);                                \
+   free(outfile);
 
 /* User specified */
 static char infile_pattern[128];
 static char outfile_pattern[128];
 
-void read_ppm_header (int fd, int header_len)
+void save_ppm_header (int fd, int header_len)
 {
    int bytesRead = 0;
    int bytesLeft = 0;
@@ -87,6 +110,44 @@ void write_output_to_file(int fdout, int num_pixels, int header_len)
    }
 }
 
+bool separate_components (UINT8 *ifile, int num_pix, UINT8 *RR, UINT8 *GG, UINT8 *BB)
+{
+   int retval = false;
+   int ii = 0, jj = 0;
+
+   if(NULL != ifile &&
+      NULL != RR &&
+      NULL != GG &&
+      NULL != BB)
+   {
+      for(ii = 0; ii < num_pix; ii++)
+      {
+         R[ii] = ifile[jj++];
+         G[ii] = ifile[jj++];
+         B[ii] = ifile[jj++];         
+      }
+
+      retval = true;
+   }
+
+   return retval;
+}
+
+void read_input_from_file(int fdin, int num_pixels, int header_len)
+{
+   if( -1 == fdin )
+   {
+      printf("Invalid File pointer passed in! Exiting!\n");
+      exit (-1);
+   }
+
+   save_ppm_header(fdin, header_len);
+
+   read(fdin, (void *)infile, num_pixels*3);
+
+   separate_components(infile, num_pixels, R, G, B);
+}
+
 bool open_files(int num, int* fdin, int* fdout)
 {
    char in_file[256], out_file[256];
@@ -116,110 +177,114 @@ bool open_files(int num, int* fdin, int* fdout)
    return true;
 }
 
+/* Our main cuda kernel */
+#if 0
+__global__ void cudaKernel (UINT8 *R, UINT8 *G, UINT8 *B,
+                            UINT8 *convR, UINT8 *convG, UINT8 *convB,
+                            int N)
+{
+   
+   if(idx < N)
+   {
+//      convR[i]=(0.30*R[i] + 0.59*G[i] + 0.11*B[i]);
+      *convR[idx] = (0.30 * R[idx]) + (0.59 * G[idx]) + (0.11 * B[idx]);
+      *convG[idx] = *convR[idx];
+      *convB[idx] = *convR[idx];
+   }
+}
+#endif
+
+void convert_to_grayscale(int num_pixels)
+{
+   int ii = 0;
+
+   // Read RGB data
+   for(ii=0; ii<num_pixels; ii++)
+   {
+      // Source: Wikipedia - http://en.wikipedia.org/wiki/Grayscale
+      convR[ii]=(0.30*R[ii] + 0.59*G[ii] + 0.11*B[ii]);
+      convG[ii]=convR[ii];
+      convB[ii]=convR[ii];
+   }
+}
+
 int main(int argc, char *argv[])
 {
-    int fdin, fdout, i;
-    int height = 0;
-    int width = 0;
-    int num_pixels = 0;
-    int header_len = 0;
-    int seq_start_num = 0;
-    int seq_count = 0;
-    int jj = 0;
+   int fdin, fdout;
+   int height = 0;
+   int width = 0;
+   int num_pixels = 0;
+   int header_len = 0;
+   int seq_start_num = 0;
+   int seq_count = 0;
+   int jj = 0;
 
-    // Estimate CPU clock rate
-    estimate_clk_rate();
+   // Estimate CPU clock rate
+   estimate_clk_rate();
     
-    if(argc != 8)
-    {
-       printf("Usage: blacknwhite <infile%%d.ppm> <width> <height> <header_len> <outfile%%d.ppm> <seq_start_num> <count>\n");
-       exit(-1);
-    }
-    else
-    {
-       width = atoi(argv[2]);
-       height = atoi(argv[3]);
-       header_len = atoi(argv[4]);
-       seq_start_num = atoi(argv[6]);
-       seq_count = atoi(argv[7]);
+   if(argc != 8)
+   {
+      printf("Usage: blacknwhite <infile%%d.ppm> <width> <height> <header_len> <outfile%%d.ppm> <seq_start_num> <count>\n");
+      exit(-1);
+   }
+   else
+   {
+      width = atoi(argv[2]);
+      height = atoi(argv[3]);
+      header_len = atoi(argv[4]);
+      seq_start_num = atoi(argv[6]);
+      seq_count = atoi(argv[7]);
 
-       num_pixels = width * height;
+      num_pixels = width * height;
 
-       printf("Using params: infile pattern: %s, outfile pattern: %s, \nheight: %d, width: %d, header_len: %d, seq_start: %d, seq_count: %d\n", argv[1], argv[5], height, width, header_len, seq_start_num, seq_count);
+      printf("Using params: infile pattern: %s, outfile pattern: %s, \nheight: %d, width: %d, header_len: %d, seq_start: %d, seq_count: %d\n", argv[1], argv[5], height, width, header_len, seq_start_num, seq_count);
 
-       // Allocate memory for holding the pixels...
-       header = (UINT8 *) malloc(header_len);
-       R = (UINT8 *) malloc(num_pixels);
-       G = (UINT8 *) malloc(num_pixels);
-       B = (UINT8 *) malloc(num_pixels);
-       convR = (UINT8 *) malloc(num_pixels);
-       convG = (UINT8 *) malloc(num_pixels);
-       convB = (UINT8 *) malloc(num_pixels);
-       outfile = (UINT8 *) malloc(header_len + num_pixels*3);
+      // Allocate memory for holding the pixels...
+      header = (UINT8 *) malloc(header_len);
+      R = (UINT8 *) malloc(num_pixels);
+      G = (UINT8 *) malloc(num_pixels);
+      B = (UINT8 *) malloc(num_pixels);
+      convR = (UINT8 *) malloc(num_pixels);
+      convG = (UINT8 *) malloc(num_pixels);
+      convB = (UINT8 *) malloc(num_pixels);
+      outfile = (UINT8 *) malloc(header_len + num_pixels*3);
+      infile = (UINT8 *) malloc(header_len + num_pixels*3);
 
-       if(NULL == header ||
-          NULL == R ||
-          NULL == G ||
-          NULL == B ||
-          NULL == convR ||
-          NULL == convB ||
-          NULL == convG ||
-          NULL == outfile)
-       {
-          printf("Could not allocate the required memory!\n");
-          exit(-1);
-       }
+      if(true != PARAMS_GOOD)
+      {
+         printf("Could not allocate the required memory!\n");
+         exit(-1);
+      }
 
-       strncpy(infile_pattern, argv[1], sizeof(infile_pattern));
-       strncpy(outfile_pattern, argv[5], sizeof(outfile_pattern));
-    }
+      strncpy(infile_pattern, argv[1], sizeof(infile_pattern));
+      strncpy(outfile_pattern, argv[5], sizeof(outfile_pattern));
+   }
 
-    for(jj=seq_start_num; jj<(seq_start_num + seq_count); jj++)
-    {
-       if( false == open_files(jj, &fdin, &fdout))
-       {
-          printf("open files failed! bailing out!\n");
-          break;
-       }
+   for(jj=seq_start_num; jj<(seq_start_num + seq_count); jj++)
+   {
+      if( false == open_files(jj, &fdin, &fdout))
+      {
+         printf("open files failed! bailing out!\n");
+         break;
+      }
 
-       read_ppm_header(fdin, header_len);
-       
+      read_input_from_file(fdin, num_pixels, header_len);
+
 /***************** Start of  core computation **************/
-       save_start_time();
-       
-       // Read RGB data
-       for(i=0; i<num_pixels; i++)
-       {
-          read(fdin, (void *)&R[i], 1); 
-          read(fdin, (void *)&G[i], 1);
-          read(fdin, (void *)&B[i], 1);
-
-          // Source: Wikipedia - http://en.wikipedia.org/wiki/Grayscale
-          convR[i]=(0.30*R[i] + 0.59*G[i] + 0.11*B[i]);
-          convG[i]=convR[i];
-          convB[i]=convR[i];
-       }
-       
-       save_stop_time();
-       print_time_info();
+      save_start_time();
+      convert_to_grayscale(num_pixels);
+      save_stop_time();
+      print_time_info();
 /***************** End of core computation **************/
-       save_start_time();
-       write_output_to_file(fdout, num_pixels, header_len);
-       save_stop_time();
-       print_time_info();
 
-       close(fdin);
-       close(fdout);
-    } // End of for loop.
+      write_output_to_file(fdout, num_pixels, header_len);
 
-    free(header); 
-    free(R); 
-    free(G); 
-    free(B); 
-    free(convR); 
-    free(convG); 
-    free(convB); 
-    free(outfile);
+      close(fdin);
+      close(fdout);
 
-    return 0;
+   } // Loop through sequence of images
+
+   FREE_MEM;
+
+   return 0;
 }
